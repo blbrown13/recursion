10227cdd7d7f05ee1f4dd3b2f1174be8db8707c1
Brandon Brown <blbrown13@users.noreply.github.com>
2017-03-19 16:49:03 -0700

    Update stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,32 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var objectType = typeof(obj);
+
+  if (Array.isArray(obj)) {
+    obj = obj.map(function(ele) {
+      return stringifyJSON(ele);
+    });
+    return '[' + obj + ']';
+  }
+
+  if (obj !== null) {
+    if (objectType === 'object') {
+      var result = [];
+
+      for (var key in obj) {
+        if (obj[key] === undefined || typeof(obj[key]) === 'function') {
+          break;
+        }
+        result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+
+      return '{' + result.join() + '}';
+
+    } else if (objectType === 'string') {
+        return '"' + obj + '"';
+    }
+  }
+
+  return '' + obj;
 };
